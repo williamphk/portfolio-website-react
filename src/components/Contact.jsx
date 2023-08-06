ebe3bafd8448bd3a73a9fd595da758261ce47c72
William Poon <willphk@outlook.com>
2023-08-06 14:43:42 -0400

    Implemented Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,3 +1,24 @@
+import "./Contact.css";
+
 export default function Contact() {
-  return <></>;
+  return (
+    <section className="contact" id="contact">
+      <h2 className="section-title">Let's Connect</h2>
+      <div className="contact-wrapper">
+        <p>Thank you for visiting my portfolio website</p>
+        <p>Feel free to contact me!</p>
+        <div>
+          <a href="mailto:willphk@outlook.com" title="My email address">
+            <i className="fa-solid fa-envelope"></i>
+          </a>
+          <a
+            href="http://linkedin.com/in/williamphk"
+            title="Link to my LinkedIn Account"
+          >
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+        </div>
+      </div>
+    </section>
+  );
 }
