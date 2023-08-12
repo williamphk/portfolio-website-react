c1c7636c2360f64423b67d3f8b6ef75df30c4343
William Poon <willphk@outlook.com>
2023-08-12 17:29:02 -0400

    Fixed Import Typo

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header.jsx";
 import About from "./components/About.jsx";
 import Projects from "./components/Projects.jsx";
 import Contact from "./components/Contact.jsx";
-import Footer from "./components/Footer/jsx";
+import Footer from "./components/Footer.jsx";
 
 export default function App() {
   return (
