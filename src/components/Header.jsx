b37828dba33cc302c233756c01991c1a29c3ea28
William Poon <willphk@outlook.com>
2023-11-27 15:28:37 -0500

    Add animation to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useState, useLayoutEffect } from "react";
+import { gsap, ScrollTrigger } from "gsap/all";
 
 import "./Header.css";
 import Menu from "../assets/menu.svg";
@@ -10,18 +11,47 @@ export default function Header() {
     setShowMenu(!showMenu);
   };
 
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    ScrollTrigger.create({
+      trigger: ".intersecting-element",
+      scrub: true,
+      start: "top 80%",
+      end: "bottom 20%",
+      onEnter: () => {
+        gsap.to("nav", { width: "85px", duration: 0.5 });
+        gsap.to(".link-name", {
+          opacity: 0,
+          duration: 0.3,
+          onComplete: () => {
+            gsap.set(".link-name", { display: "none" });
+          },
+        });
+      },
+      onLeaveBack: () => {
+        gsap.to("nav", { width: "165px", duration: 0.5 });
+        gsap.set(".link-name", { display: "flex" });
+        gsap.to(".link-name", { opacity: 1, duration: 0.3 });
+      },
+    });
+  }, []);
+
   return (
     <header>
       <nav>
         <div className={`nav-link ${showMenu ? "show-menu" : ""}`}>
           <a className="link" href="#about" onClick={toggleMenu}>
-            <i className="fa-solid fa-house"></i>HOME
+            <i className="fa-solid fa-house"></i>
+            <span className="link-name">HOME</span>
           </a>
           <a className="link" href="#work" onClick={toggleMenu}>
-            <i className="fa-solid fa-folder"></i>PROJECTS
+            <i className="fa-solid fa-folder"></i>
+            <span className="link-name">PROJECTS</span>
           </a>
           <a className="link" href="#contact" onClick={toggleMenu}>
-            <i className="fa-solid fa-address-card"></i>CONTACT
+            <i className="fa-solid fa-address-card"></i>
+            <span className="link-name">CONTACT</span>
           </a>
         </div>
         <button
